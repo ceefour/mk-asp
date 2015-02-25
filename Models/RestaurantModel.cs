5f4b5397cfb0d3acf0cf9dc7f7948c9873736738
Hendy Irawan <ceefour666@gmail.com>
2015-02-25 06:26:26 -0800

    restaurant model

---
+++ Models/RestaurantModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mk_asp.Models
+{
+	public class RestaurantModel
+	{
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String Address { get; set; }
+
+	}
+}
